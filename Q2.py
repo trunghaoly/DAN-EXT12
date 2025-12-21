b01a221052d5219e73f853f052720e8aeb0a7bbe
Lee Hao <trunghao.ly@gmail.com>
2025-12-21 17:40:16 +0930

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,39 +1,47 @@
 import pandas as pd
 import glob
+import numpy as np
 
 # Load and merge all CSV files
 file = glob.glob('C:/Users/Ly Trung Hao/OneDrive - Charles Darwin University/Units/Software now/Assignment 2/temperatures/*.csv')
 full_data = []
-
 for filename in file:
     data_temp = pd.read_csv(filename)
     full_data.append(data_temp)
 
+# Merge all data into one DataFrame
 dataframes = pd.concat(full_data, ignore_index = True)
 
-# Define seasons mapping
-seasons = {
-            'Summer': ['December', 'January', 'February'], 
-            'Autumn': ['March','April','May'],
-            'Winter':['June','July','August'],
-            'Spring':['September','October','November']
-           }
+# Convert month columns to rows
+dataframes= dataframes.set_index(['STATION_NAME','STN_ID','LAT','LON']).stack().reset_index()
+dataframes.columns = ['STATION_NAME','STN_ID','LAT','LON','Month','Temp']
 
-# Calculate and save seasonal averages
+# Define season mapping logic
+conditions = [
+                dataframes['Month'].isin(['December', 'January', 'February']),
+                dataframes['Month'].isin(['March','April','May']),
+                dataframes['Month'].isin(['June','July','August']),
+                dataframes['Month'].isin(['September','October','November']),
+]
+choices = ['Summer','Autumn','Winter','Spring']
+
+# Assign 'Season' column based on logic
+dataframes['Season'] = np.select(conditions,choices)
+
+# Calculate average temperature per season
+grouped = dataframes.groupby('Season')['Temp']
+season_ave_temp = grouped.mean().reset_index()
+
+# Save result
 with open ('average_temp.txt','w',encoding='utf-8') as f:
-    for season_name, months in seasons.items():
-        season_data = dataframes[months]
-        avg_temp = season_data.stack().mean()
-        f.write(f'{season_name}: {avg_temp:.1f}°C\n')
+    for index,row in season_ave_temp.iterrows():
+        f.write(f"{row['Season']}: {row['Temp']:.1f}°C\n")
 
-# Process station stats (months are cols 4-15)
-all_months_name = dataframes.columns[4:16]
-grouped = dataframes.groupby('STATION_NAME')[all_months_name]
-
-# Calculate Max, Min, and Range per station
+# # Calculate Max, Min, and Range per station
 station_stats = pd.DataFrame()
-station_stats['Max_Ever'] = grouped.max().max(axis=1)
-station_stats['Min_Ever'] = grouped.min().min(axis=1)
+grouped = dataframes.groupby('STATION_NAME')['Temp']
+station_stats['Max_Ever'] = grouped.max()
+station_stats['Min_Ever'] = grouped.min()
 station_stats['Range'] = station_stats['Max_Ever'] - station_stats['Min_Ever']
 max_temp_range = station_stats['Range'].max()
 
@@ -46,7 +54,7 @@ with open('largest_temp_range_station.txt', 'w', encoding='utf-8') as f:
             f.write(f'Station {station_name}: Range {max_temp_range:.1f}°C (Max: {ma:.1f}°C, Min: {mi:.1f}°C)\n')
 
 # Calculate stability (Standard Deviation)
-station_stats['Stability']=grouped.mean().std(axis = 1)
+station_stats['Stability']=grouped.std()
 min_std = station_stats['Stability'].min()
 max_std = station_stats['Stability'].max()
 
