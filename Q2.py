b7b2b501e4c4b0f4e7192a01770d06d173d744b6
Lee Hao <trunghao.ly@gmail.com>
2025-12-28 23:08:01 +0930

    update

--- Q2.py
+++ Q2.py
@@ -1,9 +1,11 @@
 import pandas as pd
-import glob
 import numpy as np
+from pathlib import Path
 
 # Load and merge all CSV files
-file = glob.glob('C:/Users/Ly Trung Hao/OneDrive - Charles Darwin University/Units/Software now/Assignment 2/temperatures/*.csv')
+base_direct = Path(__file__).resolve().parent
+data_direct = base_direct / "temperatures"
+file = sorted(data_direct.glob("stations_group_*.csv"))
 full_data = []
 for filename in file:
     data_temp = pd.read_csv(filename)
@@ -12,12 +14,19 @@ for filename in file:
 # Merge all data into one DataFrame
 dataframes = pd.concat(full_data, ignore_index = True)
 
-# Ignore missing values (NaN)
-dataframes = dataframes.dropna()
-
 # Convert month columns to rows
-dataframes= dataframes.set_index(['STATION_NAME','STN_ID','LAT','LON']).stack().reset_index()
-dataframes.columns = ['STATION_NAME','STN_ID','LAT','LON','Month','Temp']
+id_cols = dataframes.columns[0:4]
+month_cols = dataframes.columns[4:16]
+dataframes = dataframes.melt(
+    id_vars = id_cols,
+    value_vars = month_cols,
+    var_name="Month",                   
+    value_name="Temp"                   
+)
+
+# Ignore missing values (NaN)
+dataframes["Temp"] = pd.to_numeric(dataframes["Temp"], errors="coerce")
+dataframes = dataframes.dropna()
 
 # Define season mapping logic
 conditions = [
@@ -32,40 +41,41 @@ choices = ['Summer','Autumn','Winter','Spring']
 dataframes['Season'] = np.select(conditions,choices)
 
 # Calculate average temperature per season
-grouped = dataframes.groupby('Season')['Temp']
-season_ave_temp = grouped.mean().reset_index()
-season_ave_temp = season_ave_temp.iloc[[1,2,0,3]]
+grouped = dataframes.groupby("Season")["Temp"].mean().reindex(["Summer", "Autumn", "Winter", "Spring"])
+season_ave_temp = grouped.reset_index()
 
 # Save result
 with open ('average_temp.txt','w',encoding='utf-8') as f:
     for index,row in season_ave_temp.iterrows():
         f.write(f"{row['Season']}: {row['Temp']:.1f}°C\n")
 
-# # Calculate Max, Min, and Range per station
-station_stats = pd.DataFrame()
-grouped = dataframes.groupby('STATION_NAME')['Temp']
-station_stats['Max_Ever'] = grouped.max()
-station_stats['Min_Ever'] = grouped.min()
-station_stats['Range'] = station_stats['Max_Ever'] - station_stats['Min_Ever']
+# Calculate Max, Min, and Range per station
+station_stats = dataframes.groupby('STATION_NAME')['Temp'].agg(["min","max"])
+station_stats['Range'] = station_stats['max'] - station_stats['min']
 max_temp_range = station_stats['Range'].max()
 
 # Export station with largest range
 with open('largest_temp_range_station.txt', 'w', encoding='utf-8') as f:
     for station_name, row in station_stats.iterrows():
         if row['Range'] == max_temp_range:
-            mi = row['Min_Ever']
-            ma  = row['Max_Ever'] 
+            mi = row['min']
+            ma  = row['max']
             f.write(f'Station {station_name}: Range {max_temp_range:.1f}°C (Max: {ma:.1f}°C, Min: {mi:.1f}°C)\n')
 
 # Calculate stability (Standard Deviation)
+grouped = dataframes.groupby('STATION_NAME')['Temp']
 station_stats['Stability']=grouped.std()
 min_std = station_stats['Stability'].min()
 max_std = station_stats['Stability'].max()
+most_stable = station_stats[station_stats['Stability'] == min_std]
+most_variable = station_stats[station_stats['Stability'] == max_std]
 
 # Export most and least stable stations
 with open('temperature_stability_stations.txt', 'w', encoding='utf-8') as f:
-    for station_name,row in station_stats.iterrows():
-        if row['Stability'] == min_std:
-            f.write(f'Most Stable: Station {station_name}: StdDev {min_std:.1f}°C\n')
-        elif row['Stability'] == max_std:
-            f.write(f'Most Variable: Station {station_name}: StdDev {max_std:.1f}°C')
+    f.write("Most Stable:\n")
+    for station_name,row in most_stable.iterrows():
+        f.write(f'{station_name}: StdDev {min_std:.1f}°C\n')
+
+    f.write("\nMost Variable:\n")
+    for station_name,row in most_variable.iterrows():        
+        f.write(f'{station_name}: StdDev {max_std:.1f}°C\n')
